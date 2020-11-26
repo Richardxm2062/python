15c21cb60fd4984eeb5ef63f1c6fe620d2cee04b
Richardxm2062 <richardxm2062@outlook.com>
2020-11-26 18:07:57 +0800

    Update Pi(蒙特卡洛算法，多进程).py

--- Pi(蒙特卡洛算法，多进程).py
+++ Pi(蒙特卡洛算法，多进程).py
@@ -1,7 +1,7 @@
 from multiprocessing import Pool
 from random import random
 from math import sqrt
-from time import clock
+import time 
 def mtkl(Drats) :
     hits = 0
     for i in range(0,Drats):
@@ -12,13 +12,13 @@ def mtkl(Drats) :
     return hits
 if __name__ == '__main__':
     pool = Pool(12)
-    time1 = clock()
+    time1 = time.perf_counter()
     testlist = [2**27,2**27,2**27,2**27,2**27,2**27,2**27,2**27]
     hlist = pool.map(mtkl,testlist)       
     pi = 4*(sum(hlist)/2**30)
     Pool.close()    
     Pool.join()
-    time2 = clock()
+    time2 = time.perf_counter()
     print('Sub-process(es) done')
     print('Pi is ',pi)
     print('Time is',time2-time1)
