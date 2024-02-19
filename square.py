9b304ed3e98831f9cf8ca3fc07cc1cd102610088
Richardxm2062 <richardxm2062@outlook.com>
2024-02-19 17:54:14 +0800

    123

--- square.py
+++ square.py
@@ -4,21 +4,21 @@ Created on Sat Nov  3 01:08:49 2018
 
 @author: Richa
 """
+import turtle 
 
-from turtle import *
 x = 200
 
 for i in range(200):
     if x > 0:
-        seth(90)
-        fd(x)
-        seth(0)
-        fd(x)
-        x-=5
-        seth(-90)
-        fd(x)
-        seth(-180)
-        fd(x)
-        x-=5
+        turtle.setheading(90)
+        turtle.forward(x)
+        turtle.setheading(0)
+        turtle.forward(x)
+        x -= 5
+        turtle.setheading(-90)
+        turtle.forward(x)
+        turtle.setheading(-180)
+        turtle.forward(x)
+        x -= 5
     else:
-        done() #防止窗口死掉
+        turtle.done() # 防止窗口死掉
