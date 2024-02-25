52a7823c52a1fb729524c44548a9a0b55e4f9039
Richardxm2062 <richardxm2062@outlook.com>
2024-02-25 20:27:23 +0800

    1

--- HW/quick_sort.py
+++ HW/quick_sort.py
@@ -1,8 +1,5 @@
 def main() :
 
-    global ct                                          #初始化全局变量
-    ct = 0
-    
     while True :
         arr_str = input("请输入一组数组，以空格分割:")
         arr_str = arr_str.split()
@@ -17,25 +14,24 @@ def main() :
 
 
 def quick_sort(arr) :
+    
+    ct = 0
+    
     if len(arr) <= 1 :                                  
         return arr
 
     else :
-        global ct
         ct += 1
         print("第{}次调用快排".format(ct))
-        pivot = arr[0]      #pivot是一个基准数，通常取第一个
+        pivot = arr[0]                                                   #pivot是一个基准数，通常取第一个
         less_than_p = [x for x in arr[1:]  if x <= pivot]
         greater_than_p = [y for y in arr[1:]  if y > pivot]
-        print("第{}次pt为".format(ct),pivot)
-        print("第{}次lp".format(ct),less_than_p)                           #.format()为占位符,依次替换{}。使用位置为双引号后
-        print("第{}次gp".format(ct),greater_than_p)                          
-        quick_sort(less_than_p)                                           #这里将持续递归使用此函数直至小于等于的数组被排序结束
-        quick_sort(greater_than_p)
-        arr[:] = less_than_p + [pivot] + greater_than_p                   #.extend方法原数组被修改并不返回新数组，旧元素并不需要被保留
-        print("开始返回递归")
-        print("第{}次递归后arr为".format(ct),arr)
-
+        
+        quick_sort(greater_than_p)                                       #递归调用，将大于的部分再次进行排序
+        
+        #在递归里 arr引用可变参数 greater_than_p 返回后修改输入层的gp
+        arr[:] = less_than_p + [pivot] + greater_than_p                  #.extend方法原数组被修改并不返回新数组，旧元素并不需要被保留
+                                                                         
 
 if __name__ == "__main__" :
     main()
