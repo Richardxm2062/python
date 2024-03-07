6d0a808a78d211efc5cdf4a7d42bd13f56bc3975
Richardxm2062 <richardxm2062@outlook.com>
2024-03-07 14:53:35 +0800

    day

---
+++ leetcode&others/graph.py
@@ -0,0 +1,32 @@
+"""图-链表
+"""
+class Vertices :
+    def __init__(self, values) :
+        self.val = values
+
+def gr
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main() :
+    vertices = ['A','B','C','D','E']
+    values = [3,5,6,1,2]
+    
+
+
+if __name__ == "__main__" :
+    main()
